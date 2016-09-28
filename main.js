94fc471eee3eefdec57a39dc14d98e2f953fe59a
andreslaley <andreseloysv@gmail.com>
2016-09-28 14:04:26 +0200

    asd
    asd

--- main.js
+++ main.js
@@ -1,12 +1,12 @@
 var express = require('express');
 var app = express();
 var server = require('http').createServer(app);
-var io = require('socket.io')(server);
-var port = process.env.PORT || 3000;
+//var io = require('socket.io')(server);
 
-server.listen(port, function () {
-    console.log('Server listening at port %d', port);
+var io = require('socket.io')({
+    transports: ['websocket'],
 });
+io.attach(process.env.PORT || 5000);
 
 
 
