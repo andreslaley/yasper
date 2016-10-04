aaba51197975a8da3c5286b6bfe97f90a77375be
andreslaley <andreseloysv@gmail.com>
2016-10-04 10:59:14 +0200

    sad
    asd

--- main.js
+++ main.js
@@ -1,5 +1,4 @@
-//var express = require('express');
-
+var express = require('express');
 //var app = express();
 //var server = require('http').createServer(app);
 //var io = require('socket.io')(server);
@@ -8,9 +7,10 @@ var io = require('socket.io')({
     transports: ['websocket']
 });
 
+
 io.attach(process.env.PORT || 5000);
 
-//io.set('origins', 'http://minorga.skrin.de:80');
+io.set('origins', 'http://minorga.skrin.de:80');
 
 // Chatroom
 
@@ -26,46 +26,47 @@ io.on('connection', function (socket) {
             username: socket.username,
             message: data
         });
-        //insert(socket.username);
+        insert(socket.username);
     });
 
     // when the client emits 'add user', this listens and executes
-//    socket.on('add user', function (username) {
-//        if (addedUser)
-//            return;
-//
-//        // we store the username in the socket session for this client
-//        socket.username = username;
-//        ++numUsers;
-//        addedUser = true;
-//        socket.emit('login', {
-//            numUsers: numUsers
-//        });
-//        // echo globally (all clients) that a person has connected
-//        socket.broadcast.emit('user joined', {
-//            username: socket.username,
-//            numUsers: numUsers
-//        });
-//    });
+    socket.on('add user', function (username) {
+        if (addedUser)
+            return;
+
+        // we store the username in the socket session for this client
+        socket.username = username;
+        ++numUsers;
+        addedUser = true;
+        socket.emit('login', {
+            numUsers: numUsers
+        });
+        // echo globally (all clients) that a person has connected
+        socket.broadcast.emit('user joined', {
+            username: socket.username,
+            numUsers: numUsers
+        });
+    });
 
     // when the client emits 'typing', we broadcast it to others
-//    socket.on('typing', function () {
-//        socket.broadcast.emit('typing', {
-//            username: socket.username
-//        });
-//    });
+    socket.on('typing', function () {
+        socket.broadcast.emit('typing', {
+            username: socket.username
+        });
+    });
 
     // when the client emits 'stop typing', we broadcast it to others
-//    socket.on('stop typing', function () {
-//        socket.broadcast.emit('stop typing', {
-//            username: socket.username
-//        });
-//    });
+    socket.on('stop typing', function () {
+        socket.broadcast.emit('stop typing', {
+            username: socket.username
+        });
+    });
 
     // when the user disconnects.. perform this
     socket.on('disconnect', function () {
         if (addedUser) {
             --numUsers;
+
             // echo globally that this client has left
             socket.broadcast.emit('user left', {
                 username: socket.username,
