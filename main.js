07de67906c2c7a7a41f8e22930361b9e93ff8103
andreslaley <andreseloysv@gmail.com>
2016-10-04 10:56:09 +0200

    sad
    sad

--- main.js
+++ main.js
@@ -26,27 +26,27 @@ io.on('connection', function (socket) {
             username: socket.username,
             message: data
         });
-        insert(socket.username);
+        //insert(socket.username);
     });
 
     // when the client emits 'add user', this listens and executes
-    socket.on('add user', function (username) {
-        if (addedUser)
-            return;
-
-        // we store the username in the socket session for this client
-        socket.username = username;
-        ++numUsers;
-        addedUser = true;
-        socket.emit('login', {
-            numUsers: numUsers
-        });
-        // echo globally (all clients) that a person has connected
-        socket.broadcast.emit('user joined', {
-            username: socket.username,
-            numUsers: numUsers
-        });
-    });
+//    socket.on('add user', function (username) {
+//        if (addedUser)
+//            return;
+//
+//        // we store the username in the socket session for this client
+//        socket.username = username;
+//        ++numUsers;
+//        addedUser = true;
+//        socket.emit('login', {
+//            numUsers: numUsers
+//        });
+//        // echo globally (all clients) that a person has connected
+//        socket.broadcast.emit('user joined', {
+//            username: socket.username,
+//            numUsers: numUsers
+//        });
+//    });
 
     // when the client emits 'typing', we broadcast it to others
 //    socket.on('typing', function () {
